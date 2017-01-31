3714aafff9d2d53517cc13f1ab10ee474d361957
Lenny <4570719@qq.com>
2017-01-31 12:03:21 +0800

    create project

---
+++ BuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJie
+//
+//  Created by Lenny on 2017/1/31.
+//  Copyright © 2017年 Lenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
